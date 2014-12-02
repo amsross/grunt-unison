c2d9107b4fc95c93016963a04f8a482b7ae67f8e
Matt Ross <matt@mattross.io>
2014-12-02 17:55:42 +0000

    Create task and runner

--- tasks/unison.js
+++ tasks/unison.js
@@ -10,41 +10,63 @@
 
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+	grunt.registerTask('unison', 'runs unison to keep hosts in sync', function() {
+		var options = this.options({
+				args: [],
+				verbose: false,
+				src: false,
+				dest: false,
+			}),
+			done = this.async(),
+			verbose = options.verbose,
+			args = options.args.concat([
+				'-auto',
+				'-batch',
+				'-confirmbigdel=false',
+				'-times',
+			]),
+			src = options.src,
+			dest = options.dest
+			;
 
-  grunt.registerMultiTask('unison', 'runs unison to keep hosts in sync', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
-
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
-  });
-
+		if (!src || !dest) {
+			grunt.log.error('Both source and destination must be specified.');
+			return false;
+		} else {
+			grunt.util.spawn({
+				args: [src, dest].concat(args),
+				cmd: 'unison',
+				opts: {
+					stdio: verbose ? [
+						process.stdin,
+						process.stout,
+						process.stderr
+					] : []
+				}
+			}, function(error, result, code) {
+				if (error) {
+					grunt.log.error(error);
+					done(false);
+					return false;
+				}
+				if(code === 127) {
+					grunt.log.error('Synchronization failed. Please consult unison log. (1)');
+					done(false);
+					return false;
+				}
+				if (result) {
+					var slice_index = result.stderr.search(/Synchronization complete at|Nothing to do/);
+					if (slice_index > -1 || verbose) {
+						grunt.log.ok(verbose ? result.stdout : result.stderr.slice(slice_index));
+						done();
+						return true;
+					} else {
+						grunt.log.error('Synchronization failed. Please consult unison log. (2)');
+						done(false);
+						return false;
+					}
+				}
+			});
+		}
+	});
 };
